ae5d1499e7da27989fd7d8b990eb81212013179d
sanjay.chouhan180 <sanjay.chouhan180@webkul.com>
2019-02-16 14:53:34 +0530

    fix for magento 2.2.5

--- src/app/code/Webkul/LayeredNavigation/Model/ResourceModel/Layer/Filter/Price.php
+++ src/app/code/Webkul/LayeredNavigation/Model/ResourceModel/Layer/Filter/Price.php
@@ -1,10 +1,6 @@
 <?php
 namespace Webkul\LayeredNavigation\Model\ResourceModel\Layer\Filter;
 
-use Magento\Framework\App\Http\Context;
-use Magento\Framework\Indexer\DimensionFactory;
-use Magento\Framework\Search\Request\IndexScopeResolverInterface;
-
 class Price extends \Magento\Catalog\Model\ResourceModel\Layer\Filter\Price 
 {
     public function __construct(
@@ -13,11 +9,8 @@ class Price extends \Magento\Catalog\Model\ResourceModel\Layer\Filter\Price
         \Webkul\LayeredNavigation\Model\Layer\Resolver $layerResolver,
         \Magento\Customer\Model\Session $session,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
-        $connectionName = null,
-        IndexScopeResolverInterface $priceTableResolver = null,
-        Context $httpContext = null,
-        DimensionFactory $dimensionFactory = null
+        $connectionName = null
     ) {
-        parent::__construct($context, $eventManager, $layerResolver, $session, $storeManager, $connectionName, $priceTableResolver, $httpContext, $dimensionFactory);
+        parent::__construct($context, $eventManager, $layerResolver, $session, $storeManager, $connectionName);
     }
 }
